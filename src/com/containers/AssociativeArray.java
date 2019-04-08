2cae1eebc4a9960ee6ada67748cd0e6415bfaa25
bofei <bofei222@163.com>
2019-04-08 18:33:05 +0800

    Map

--- src/com/containers/AssociativeArray.java
+++ src/com/containers/AssociativeArray.java
@@ -14,26 +14,29 @@ public class AssociativeArray<K,V> {
             throw new ArrayIndexOutOfBoundsException();
         pairs[index++] = new Object[]{ key, value };
     }
+
     @SuppressWarnings("unchecked")
     public V get(K key) {
         for (int i = 0; i < index; i++)
             if (key.equals(pairs[i][0]))
                 return (V)pairs[i][1];
-        return null; // Did not find key
+        return null;
     }
+
     public String toString() {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < index; i++) {
-            result.append(pairs[i][0]).toString();
+            result.append(pairs[i][0].toString());
             result.append(" : ");
-            result.append(pairs[i][1]).toString();
-            if (i < index -1)
+            result.append(pairs[i][1].toString());
+            if (i < index - 1)
                 result.append("\n");
         }
         return result.toString();
+
     }
     public static void main(String[] args) {
-        AssociativeArray<String,String> map = 
+        AssociativeArray<String,String> map =
             new AssociativeArray<String,String>(6);
         map.put("sky", "blue");
         map.put("grass", "green");
--- src/com/containers/Maps.java
+++ src/com/containers/Maps.java
@@ -13,45 +13,35 @@ import java.util.concurrent.ConcurrentHashMap;
 import net.mindview.util.CountingMapData;
 
 public class Maps {
-    public static void printKeys(Map<Integer,String> map) {
+    public static void printKeys(Map<Integer, String> map) {
         printnb("Size = " + map.size() + ", ");
-        printnb("Keys: ");
+        printnb("Keys: " );
         print(map.keySet());
     }
-    public static void test(Map<Integer,String> map) {
-        print(map.getClass().getSimpleName());
+
+    public static void test(Map<Integer, String> map) {
+        print(map.getClass().getSuperclass());
         map.putAll(new CountingMapData(25));
-        // Map has 'Set'
+        //
         map.putAll(new CountingMapData(25));
         printKeys(map);
-        // Producing a Collection of values:
+        //
         printnb("Values: ");
         print(map.values());
         print(map);
         print("map.containsKey(11): " + map.containsKey(11));
         print("map.get(11): " + map.get(11));
-        print("map.containsValue(\"F0\"): " +
-            map.containsValue("F0"));
-        Integer key = map.keySet().iterator().next();
-        print("First key in map: " + key);
-        map.remove(key);
-        printKeys(map);
-        map.clear();
-        print("map.isEmpty(): " + map.isEmpty());
-        map.putAll(new CountingMapData(25));
-        // Operations on the Set change the Map:
-        map.keySet().removeAll(map.keySet());
-        print("map.isEmpty(): " + map.isEmpty());
-        
+        print("map.containsValue(\"F0\")" + map.containsValue("F0"));
+
+
     }
+
     public static void main(String[] args) {
-//        test(new HashMap<Integer,String>());
-//        test(new TreeMap<Integer,String>());
+        test(new HashMap<Integer,String>());
+        test(new TreeMap<Integer,String>());
         test(new LinkedHashMap<Integer,String>());
-//        test(new IdentityHashMap<Integer,String>());
-//        test(new ConcurrentHashMap<Integer,String>());
-//        test(new WeakHashMap<Integer,String>());
-        
-        
+        test(new IdentityHashMap<Integer,String>());
+        test(new ConcurrentHashMap<Integer,String>());
+        test(new WeakHashMap<Integer,String>());
     }
 }
